b3efeb05dd5e72f56aff718174082a6c64f9d69d
Drew <drewclementsdesign@gmail.com>
2020-09-30 00:39:21 -0400

    tinkering with header

--- components/Global/TNHeader.vue
+++ components/Global/TNHeader.vue
@@ -1,5 +1,13 @@
 <template>
-	<header> </header>
+	<header class="relative top-0 w-full h-12 bg-black">
+		<div class="container mx-auto px-5">
+			<router-link to="/">
+				<img src="~static/images/tnn_logo.jpg" class="absolute w-32 z-50" />
+			</router-link>
+		</div>
+
+		<img src="~assets/svg/header-bg.svg" class="absolute fill-current mt-5" />
+	</header>
 </template>
 
 <script>
--- pages/index.vue
+++ pages/index.vue
@@ -1,11 +1,16 @@
 <template>
-	<div class="container">
-		Hello
+	<div style="height: 1200px">
+		<t-n-header />
 	</div>
 </template>
 
 <script>
+	import TNHeader from '@/components/Global/TNHeader';
+
 	export default {
 		name: 'Home',
+		components: {
+			TNHeader,
+		},
 	};
 </script>
