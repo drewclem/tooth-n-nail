20ce31725488c7c20ab2624b61bb47bc019c1416
Drew <drewclementsdesign@gmail.com>
2022-03-09 22:51:52 -0500

    oops

--- nuxt.config.js
+++ nuxt.config.js
@@ -52,7 +52,7 @@ export default {
 			{
 				hid: 'twitter:image',
 				property: 'twitter:image',
-				content: 'tntparlour.com/og-image.png',
+				content: 'https://tntparlour.com/og-image.png',
 			},
 		],
 		link: [{ rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }],
